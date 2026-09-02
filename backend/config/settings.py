9ea5824a7e7983261c5d4db528264346becb67a9
programReg <1reggiejoseph@gmail.com>
2026-09-02 02:49:02 -0400

    Add Render domain to ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["car-inventory-manage-sys.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
